b7e6ff6aad965266459bd022ceea24495385fa58
Mark Gituma <gitumarkk@gmail.com>
2013-07-30 22:37:40 +0200

    adding registration open context processor

--- umonya/umonya/apps/main/context_processors.py
+++ umonya/umonya/apps/main/context_processors.py
@@ -1,7 +1,8 @@
 def dynamic_menu(request):
     # Adds Dynamic Menu Template Context
-    from models import Dynamic_Section
+    from models import Registration
     try:
-        return{"registration": Dynamic_Section.objects.get(section="registration")}
-    except:
-        return {"registration": False}
+        Registration.objects.get(pk=1)
+        return{"registration_open": True}
+    except Registration.DoesNotExist:
+        return {"registration_open": False}
